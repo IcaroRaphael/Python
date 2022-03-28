0c278a43f8e28e2127032c84eb83dc1096fbfa2c
Ícaro Raphael Queiroz Cavalcante <icaro2551@gmail.com>
2022-03-28 19:59:46 -0300

    1.0

--- Faculdade/Estrutura De Dados/TDE02/ex003.py
+++ Faculdade/Estrutura De Dados/TDE02/ex003.py
@@ -2,7 +2,11 @@
 3.Reverso do número. Faça uma função que retorne o reverso de um número inteiro
 informado
 """
-num = int(input("Insira um número: "))
-string = str(num)
-num = int(string[::-1])
-print(f"Reverso do número: {num}")
+def reverso():
+    num = int(input("Insira um número: "))
+    string = str(num)
+    num = int(string[::-1])
+    print(f"Reverso do número: {num}")
+
+
+reverso()
